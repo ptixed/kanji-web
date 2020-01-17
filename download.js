6175dc063323afc92a9816f9edc0796ec11c82f9
ptixed <ptixed@users.noreply.github.com>
2020-01-17 17:50:03 +0100

    progress

--- download.js
+++ download.js
@@ -1,4 +1,4 @@
-let _ = require("lodash");
+let fs = require("fs");
 let request = require("request-promise");
 let json = require("json-cyclic");
 
@@ -8,18 +8,25 @@ if (!token) {
     process.exit(1);
 }
 
-// load file or create new 
+console.error('Starting download with token ' + token.substr(0,1) + '***' + token.substr(-1));
+getpage(process.env.WANIKANIURL || "https://api.wanikani.com/v2/subjects");
+
 var data = {
     timestamp: Date.now(),
-    kanjis: [],
-    radicals: [],
-    words: []
+    words: {},
+    kanjis: {},
+    radicals: {}
 };
 
 function getpage(url) {
-
-    if (!url)
+    if (!url) {
+        for (var id in data.kanjis) {
+            data.kanjis[id].radicals = data.kanjis[id].radicals.map(x => data.radicals[x]);
+        }
+        fs.writeFile('db.json', JSON.stringify(json.encycle(data)));
         return;
+    }
+    
     console.error(url);
     
     request.get({
@@ -31,22 +38,49 @@ function getpage(url) {
     })
     .then(response => {
         console.log(response);
+        response = JSON.parse(response);
         
         for (var item of response.data) {
             switch (item.object) {
                 case 'radical':
+                    data.radicals[item.id] = {
+                        name: item.data.slug,
+                        value: item.data.characters
+                    };
                     break;
                 case 'kanji':
+                    data.kanjis[item.id] = {
+                        value: item.data.characters,
+                        meanings: item.data.meanings
+                            .sort((x, y) => {
+                                if (x.primary) return 1;
+                                if (y.primary) return -1;
+                                return 0;
+                            })
+                            .map(x => x.meaing),
+                        onyomi: item.data.readings
+                            .filter(x => x.type == 'onyomi')
+                            .map(x => ({
+                                primary: x.primary,
+                                reading: x.reading
+                            })),
+                        kunyomi: item.data.readings
+                            .filter(x => x.type == 'kunyomi')
+                            .map(x => ({
+                                primary: x.primary,
+                                reading: x.reading
+                            })),
+                        radicals: item.data.component_subject_ids,
+                        // vocabs = item.data.amalgamation_subject_ids
+                    };
                     break;
                 case 'vocabulary':
-                    break;
+                    console.log(JSON.stringify(item));
+                    return;
             }
         }
         
-        //getpage(response.pages.next_url);
+        getpage(response.pages.next_url);
     });
     
 }
-
-console.error('Starting download with token ' + token.substr(0,1) + '***' + token.substr(-1));
-getpage(process.env.WANIKANIURL || "https://api.wanikani.com/v2/subjects");
